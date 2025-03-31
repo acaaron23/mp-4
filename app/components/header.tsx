9d1d98773cc85b20fc462077f222468529c492e4
Aaron Chen <acaaron@bu.edu>
2025-03-31 01:43:58 -0400

    Small tweaks

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 export default function Header() {
-    const linkStyling = "p-4 text-white text-2xl hover:underline";
+    const linkStyling = "p-4 font-semibold text-white text-2xl hover:underline";
     return (
         <header className="flex justify-between items-center h-20 bg-black">
             <h2 className="text-3xl font-semibold p-4 m-2 text-white"> Validity of Phone Numbers </h2>
--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
         <div className="flex flex-col items-center justify-center bg-blue-200 h-200">
             <h1 className="text-9xl font-bold mb-4">Welcome!</h1>
             <p className="text-3xl">
-                Click on <b>Number Checker</b> on the top to check the validity of a phone number.
+                Click on <b>Number Checker</b> at the top to check the validity of a phone number.
             </p>
         </div>
     );
