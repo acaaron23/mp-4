5138aa7191f7a6b3e784996682d5b74cb7aec1f1
Aaron Chen <acaaron@bu.edu>
2025-04-02 17:00:12 -0400

    Changes to styling and buttons

--- app/numbercheck/page.tsx
+++ app/numbercheck/page.tsx
@@ -8,7 +8,7 @@ export default function NumberCheck(){
 
     return (
         <div className="flex flex-col items-center justify-center bg-blue-200 h-200">
-            <div className="flex flex-col bg-white border-black border-3 p-15 h-90 rounded-4xl">
+            <div className="flex flex-col items-center bg-white border-black border-3 p-15 h-90 rounded-4xl">
                 <h1 className="font-bold text-5xl text-black mb-3">
                     Phone Number Verification
                 </h1>
@@ -22,10 +22,8 @@ export default function NumberCheck(){
                     className = "w-full bg-white border-black border-4 rounded-2xl text-3xl p-2 mt-2"
                     onChange={(e) => setNumber(e.target.value)}
                 />
-                <Link href={`/${number}`}>
-                    <button type="button" className="w-full text-white text-3xl rounded-xl bg-green-600 hover:bg-green-500 p-2 mt-2">
-                        Check Number
-                    </button>
+                <Link href={`/${number}`} className="w-1/2 text-white text-3xl text-center rounded-xl bg-green-600 hover:bg-green-500 p-2 mt-2">
+                    Check Number
                 </Link>
             </div>
         </div>
